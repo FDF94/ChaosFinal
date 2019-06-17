2d22a6598feeaeac4878c8bb536f3887e395230f
FDF94 <fdelfedele@gmail.com>
2019-06-17 10:43:23 -0300

    Omega and A are well calculated

--- calculus.py
+++ calculus.py
@@ -11,26 +11,29 @@ def derivateA(F, Omega):
     return F*Omega
 
 
-def derivateF(A, F, L, dAdt):
+def derivateF(A, F, L, Omega):
+    dAdt = derivateA(F, Omega)
     result = (1-F)*dAdt + 2*L
     return result/A
 
 
-def derivateOmega(A, E, F, L, Omega, dAdt, dFdt, dLdt):
-    Psi = (3 * Omega**2 + 1)*(1-F) + 2*E*(1+Omega)
-    term1_factor1 = 3*(L + 3*(1-F)*dAdt/2)/(A*F) + dFdt - dAdt*Psi
-    term1_factor2 = L + 3*(1-F) *dAdt / 2
+def derivateOmega(t, A, E, F, L, Omega):
+
+    dAdt = derivateA(F, Omega)
+    dFdt = derivateF(A, F, L, Omega)
+    dLdt = derivateL(t)
+    Alpha = 1-F
+    Psi = (3 * Omega**2 + 1)* Alpha+ 2*E*(1+Omega)
+     
+    term1_factor1 = 3*(L + 3*(Alpha)*dAdt/2)/(A*F) + dFdt - dAdt*Psi
+    term1_factor2 = L + 3*Alpha *dAdt / 2
     term2 = 6*L*dAdt/A
-    term3 = 6 * (1-F)* dAdt**2 / A
+    term3 = 6 * Alpha* dAdt**2 / A
     term4 = dLdt
-    term5 = 3 * (1 - F) * dFdt * Omega
-    term6 = F**2 * (Psi - (1 - F)) / A**2
+    term5 = 3 * Alpha * dFdt * Omega
+    term6 = F**2 * (Psi - Alpha) / A**2
 
     return term1_factor1 * term1_factor2 + term2 + term3 - term4 - term5 - term6
-
-
-def derivateL():
-    return 1
 
 
 def next_A_value(current_A, F, Omega, h = 0.1):
@@ -53,20 +56,20 @@ def next_F_value(current_F, A, L, dAdt, h = 0.1):
     return nextF 
 
 
-def next_Omega_value(A, E, F, L, current_Omega, dAdt, dFdt, dLdt = 0, h = 0.1):
-    k1 = derivateOmega(A, E, F, L, current_Omega, dAdt, dFdt, dLdt)
-    k2 = derivateOmega(A + 0.5 * h * k1, E + 0.5 * h * k1, F + 0.5 * h * k1, 
-        L + 0.5 * h * k1, current_Omega * 0.5 * h, dAdt + 0.5 * h * k1, 
-        dFdt + 0.5 * h * k1, dLdt + 0.5 * h * k1)
-    k3 = derivateOmega(A + 0.5 * h * k2, E + 0.5 * h * k2, F + 0.5 * h * k2, 
-        L + 0.5 * h * k2, current_Omega * 0.5 * h, dAdt + 0.5 * h * k2, 
-        dFdt + 0.5 * h * k2, dLdt + 0.5 * h * k2)
-    k4 = derivateOmega(A + h * k3, E + h * k3, F + h * k3, 
-        L + h * k3, current_Omega * h, dAdt + h * k3, 
-        dFdt + h * k3, dLdt + h * k3)
-    nextF = current_Omega + (k1 + 2*k2 + 2*k3 + k4) * h/6
+# def next_Omega_value(A, E, F, L, current_Omega, dAdt, dFdt, dLdt = 0, h = 0.1):
+#     k1 = derivateOmega(A, E, F, L, current_Omega, dAdt, dFdt, dLdt)
+#     k2 = derivateOmega(A + 0.5 * h * k1, E + 0.5 * h * k1, F + 0.5 * h * k1, 
+#         L + 0.5 * h * k1, current_Omega * 0.5 * h, dAdt + 0.5 * h * k1, 
+#         dFdt + 0.5 * h * k1, dLdt + 0.5 * h * k1)
+#     k3 = derivateOmega(A + 0.5 * h * k2, E + 0.5 * h * k2, F + 0.5 * h * k2, 
+#         L + 0.5 * h * k2, current_Omega * 0.5 * h, dAdt + 0.5 * h * k2, 
+#         dFdt + 0.5 * h * k2, dLdt + 0.5 * h * k2)
+#     k4 = derivateOmega(A + h * k3, E + h * k3, F + h * k3, 
+#         L + h * k3, current_Omega * h, dAdt + h * k3, 
+#         dFdt + h * k3, dLdt + h * k3)
+#     nextF = current_Omega + (k1 + 2*k2 + 2*k3 + k4) * h/6
 	
-    return nextF 
+#     return nextF 
 
 
 def derivateL(t):
@@ -82,26 +85,60 @@ def Gaussian(t):
 
 
 def main():
-    t = np.arange(0, 100, 0.1)
-    A = np.ones(1000, float) * 5
-    E = 1
-    F = np.ones(1000, float) * 0.6
-    L = 0.01
-    Omega = np.zeros(1000, float)
-    dAdt = F*Omega
-    dFdt = derivateF(A, F, L, dAdt)
+    #Initialize hyperparameters
+    points_number = 10000
+    h = 0.01 #step size
 
-    for i in range(499):
-        A[i+1] = next_A_value(A[i], F[i], Omega[i])
-        F[i+1] = next_F_value(F[i], A[i], L, dAdt[i])
-        Omega[i+1] = next_Omega_value(A[i], E, F[i], L, Omega[i],
-            dAdt[i],dFdt[i])
-        dAdt[i+1] = derivateA(F[i+1], Omega[i+1])
-        dFdt[i+1] = derivateF(A[i+1], F[i+1], L, dAdt[i+1])
+    # Initialize all variables
+    E_0 = 1
+    t = np.arange(0, points_number * h, h)
+    A = np.ones(points_number, float) * 5
+    Omega = np.zeros(points_number, float)
+    E = E_0*(1+Omega)
+    F = np.ones(points_number, float) * 0.6
+    L = Gaussian(t)
+
+    # loop over all points
+    for i in range(points_number - 1):
+        kA1 = derivateA(F[i],Omega[i])
+        kF1 = derivateF(A[i], F[i], L[i], Omega[i])
+        kO1 = derivateOmega(t[i], A[i], E[i], F[i], L[i], Omega[i])
+        kl1 = derivateL(t[i])
+
+        kA2 = derivateA(F[i] + 0.5 * h * kF1,Omega[i] + 0.5 * h * kO1)
+        kF2 = derivateF(A[i] + 0.5 * h * kA1, F[i]+ 0.5 * h * kF1, 
+            L[i]+ 0.5 * h * kl1, Omega[i] + 0.5 * h * kO1)
+        kO2 = derivateOmega(t[i] + 0.5 * h, A[i] + 0.5 * h * kA1, 
+            E[i], F[i] + 0.5 * h * kF1, L[i] + 0.5 * h * kl1,
+            Omega[i] + 0.5 * h * kO1)
+        kl2 = derivateL(t[i] + 0.5*h)
+
+        kA3 = derivateA(F[i] + 0.5 * h * kF2,Omega[i] + 0.5 * h * kO2)
+        kF3 = derivateF(A[i] + 0.5 * h * kA2, F[i]+ 0.5 * h * kF2, 
+            L[i]+ 0.5 * h * kl2, Omega[i] + 0.5 * h * kO2)
+        kO3 = derivateOmega(t[i] + 0.5 * h, A[i] + 0.5 * h * kA2, 
+            E[i], F[i] + 0.5 * h * kF2, L[i] + 0.5 * h * kl2,
+            Omega[i] + 0.5 * h * kO2)
+        kl3 = derivateL(t[i] + 0.5*h)
+
+        kA4 = derivateA(F[i] + h, Omega[i] + h * kA3)
+        kF4 = derivateF(A[i] + h * kA3, F[i]+ h * kF3, 
+            L[i]+ h * kl3, Omega[i] + h * kO3)
+        kO4 = derivateOmega(t[i] + h, A[i] + h * kA3, 
+            E[i], F[i] + h * kF3, L[i] + h * kl3,
+            Omega[i] + h * kO3)
+        kl4 = derivateL(t[i] + h)
+    
+        A[i+1] = A[i] + (kA1 + 2*kA2 + 2*kA3 + kA4) * h/6
+        F[i+1] = F[i] + (kF1 + 2*kF2 + 2*kF3 + kF4) * h/6
+        Omega[i+1] = Omega[i] + (kO1 + 2*kO2 + 2*kO3 + kO4) * h/6
+        E[i+1] = E_0*(1+Omega[i])
+        
         print(i)
 
-    plt.plot(t, A)
+
     plt.plot(t, Omega)
+    plt.plot(t, A)
     plt.show()
     
 

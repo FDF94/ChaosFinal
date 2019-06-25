13affe8fe17c41c6425691d3cd8a4319185e455e
FDF94 <fdelfedele@gmail.com>
2019-06-24 21:06:42 -0300

    Added more arguments

--- calculus.py
+++ calculus.py
@@ -36,11 +36,11 @@ def derivateF(A, F, L, Omega):
     return result/A
 
 
-def derivateOmega(t, A, F, L, Omega):
+def derivateOmega(t, A, F, L, Omega, t0, L0, sigma):
 
     dAdt = derivateA(F, Omega)
     dFdt = derivateF(A, F, L, Omega)
-    dLdt = derivateL(t)
+    dLdt = derivateL(t, t0, L0, sigma)
     Alpha = 1-F
     Psi = (3 * Omega**2 + 1) * Alpha + 2*L*(1+Omega)/F
 
@@ -57,26 +57,20 @@ def derivateOmega(t, A, F, L, Omega):
             term3 - term4 - term5 - term6)*common_factor
 
 
-def derivateL(t):
-    L0 = 0.05
-    sigma = 1
-    t0 = 20
+def derivateL(t, t0, L0, sigma):
     return -L0*(t - t0)*np.exp(-((t - t0)**2)/(2*sigma**2))/(np.sqrt(2*np.pi)*sigma**(5/2))
 
 
-def Gaussian(t):
-    L0 = 0.05
-    sigma = 1
-    t0 = 20
+def Gaussian(t, t0, L0, sigma):
     return L0*np.exp(-((t - t0)**2)/(2*sigma**2))/(np.sqrt(2*np.pi*sigma))
 
 
-def get_results(points_number, h, a0, f0, omega0, t):
+def get_results(points_number, h, a0, f0, omega0, t, t0, L0, sigma):
     # Initialize all variables
     A = np.ones_like(t, float) * a0
     Omega = np.ones_like(t, float) * omega0
     F = np.ones_like(t, float) * f0
-    L = Gaussian(t)
+    L = Gaussian(t, t0, L0, sigma)
 
     with(np.errstate(invalid='raise', over='raise')):
         # loop over all points
@@ -84,32 +78,31 @@ def get_results(points_number, h, a0, f0, omega0, t):
             for i in range(points_number - 1):
                 kA1 = derivateA(F[i], Omega[i])
                 kF1 = derivateF(A[i], F[i], L[i], Omega[i])
-                kO1 = derivateOmega(t[i], A[i], F[i], L[i], Omega[i])
-                kl1 = derivateL(t[i])
+                kO1 = derivateOmega(t[i], A[i], F[i], L[i], Omega[i], t0, L0, sigma)
+                kl1 = derivateL(t[i], t0, L0, sigma)
 
                 kA2 = derivateA(F[i] + 0.5 * h * kF1, Omega[i] + 0.5 * h * kO1)
                 kF2 = derivateF(A[i] + 0.5 * h * kA1, F[i] + 0.5 * h * kF1,
                                 L[i] + 0.5 * h * kl1, Omega[i] + 0.5 * h * kO1)
                 kO2 = derivateOmega(t[i] + 0.5 * h, A[i] + 0.5 * h * kA1,
                                     F[i] + 0.5 * h * kF1, L[i] + 0.5 * h * kl1,
-                                    Omega[i] + 0.5 * h * kO1)
-                kl2 = derivateL(t[i] + 0.5*h)
+                                    Omega[i] + 0.5 * h * kO1, t0, L0, sigma)
+                kl2 = derivateL(t[i] + 0.5*h, t0, L0, sigma)
 
                 kA3 = derivateA(F[i] + 0.5 * h * kF2, Omega[i] + 0.5 * h * kO2)
                 kF3 = derivateF(A[i] + 0.5 * h * kA2, F[i] + 0.5 * h * kF2,
                                 L[i] + 0.5 * h * kl2, Omega[i] + 0.5 * h * kO2)
                 kO3 = derivateOmega(t[i] + 0.5 * h, A[i] + 0.5 * h * kA2,
                                     F[i] + 0.5 * h * kF2, L[i] + 0.5 * h * kl2,
-                                    Omega[i] + 0.5 * h * kO2)
-                kl3 = derivateL(t[i] + 0.5*h)
+                                    Omega[i] + 0.5 * h * kO2, t0, L0, sigma)
+                kl3 = derivateL(t[i] + 0.5*h, t0, L0, sigma)
 
                 kA4 = derivateA(F[i] + h, Omega[i] + h * kA3)
                 kF4 = derivateF(A[i] + h * kA3, F[i] + h * kF3,
                                 L[i] + h * kl3, Omega[i] + h * kO3)
                 kO4 = derivateOmega(t[i] + h, A[i] + h * kA3,
                                     F[i] + h * kF3, L[i] + h * kl3,
-                                    Omega[i] + h * kO3)
-                kl4 = derivateL(t[i] + h)
+                                    Omega[i] + h * kO3, t0, L0, sigma)
 
                 A[i+1] = A[i] + (kA1 + 2*kA2 + 2*kA3 + kA4) * h/6
                 F[i+1] = F[i] + (kF1 + 2*kF2 + 2*kF3 + kF4) * h/6
@@ -123,11 +116,11 @@ def get_results(points_number, h, a0, f0, omega0, t):
         return A, F, L, Omega
 
 
-def main(a0, f0, omega0, points_number, step_size):
+def main(a0, f0, omega0, t0, L0, sigma, points_number, step_size):
 
     t = np.arange(0, points_number * step_size, step_size)
 
-    A, F, L, Omega = get_results(points_number, step_size, a0, f0, omega0, t)
+    A, F, L, Omega = get_results(points_number, step_size, a0, f0, omega0, t, t0, L0, sigma)
 
     M = (1-F)*A/2
 
@@ -163,6 +156,12 @@ parser.add_argument("-O", "--omega0", default=0,
 parser.add_argument("-s", "--step_size", default=0.01,
                     help="Step size for calculus", type=restricted_A)
 parser.add_argument("-pn", "--points_number", default=10000,
-                    help="Number of points to calculate", type=restricted_A)                                                                            
+                    help="Number of points to calculate", type=int)
+parser.add_argument("-S", "--sigma", default=10,
+                    help="Width for the gaussian function", type=float)          
+parser.add_argument("-L", "--L0", default=0.05,
+                    help="Maximum value for the gaussian function", type=float)     
+parser.add_argument("-t", "--t0", default=10,
+                    help="Time when gaussian will reach its maximum value", type=float)                                                                            
 args = parser.parse_args()
-main(args.a0, args.f0, args.omega0, args.points_number, args.step_size)
+main(args.a0, args.f0, args.omega0, args.t0, args.L0, args.sigma, args.points_number, args.step_size)

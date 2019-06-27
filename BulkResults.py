80efed77e64e643eb402827b7d0cd1c9abaf1fb4
FDF94 <fdelfedele@gmail.com>
2019-06-26 22:38:06 -0300

    Bulk now work with arguments

--- BulkResults.py
+++ BulkResults.py
@@ -1,25 +1,12 @@
 # Matemática
 import numpy as np
-from calculus import get_results
+from functions import *
 # Gráficos
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 plt.style.use("seaborn-whitegrid")
 # System
 import argparse
-
-
-def restricted_F(x):
-    y=float(x)
-    if y <= 0.0 or y >= 1.0:
-        raise argparse.ArgumentTypeError("%r not in range (0.0, 1.0)"%(y,))
-    return y
-
-def restricted_Omega(x):
-    y=float(x)
-    if y <= -1.0 or y >= 1.0:
-        raise argparse.ArgumentTypeError("%r not in range (-1.0, 1.0)"%(y,))
-    return y
 
 def positive_float(x):
     y=float(x)
@@ -28,20 +15,70 @@ def positive_float(x):
     return y
 
 
-def main(a0, f0, omega0, t0, L0, sigma, points_number, step_size):
+def get_results(points_number, h, a0, f0, omega0, t, t0, L0, sigma):
+    # Initialize all variables
+    A = np.ones_like(t, float) * a0
+    Omega = np.ones_like(t, float) * omega0
+    F = np.ones_like(t, float) * f0
+    L = Gaussian(t, t0, L0, sigma)
+
+    with(np.errstate(invalid='raise', over='raise')):
+        # loop over all points
+        for i in range(points_number - 1):
+            kA1 = derivateA(F[i], Omega[i])
+            kF1 = derivateF(A[i], F[i], L[i], Omega[i])
+            kO1 = derivateOmega(t[i], A[i], F[i], L[i], Omega[i], t0, L0, sigma)
+            kl1 = derivateL(t[i], t0, L0, sigma)
+
+            kA2 = derivateA(F[i] + 0.5 * h * kF1, Omega[i] + 0.5 * h * kO1)
+            kF2 = derivateF(A[i] + 0.5 * h * kA1, F[i] + 0.5 * h * kF1,
+                            L[i] + 0.5 * h * kl1, Omega[i] + 0.5 * h * kO1)
+            kO2 = derivateOmega(t[i] + 0.5 * h, A[i] + 0.5 * h * kA1,
+                                F[i] + 0.5 * h * kF1, L[i] + 0.5 * h * kl1,
+                                Omega[i] + 0.5 * h * kO1, t0, L0, sigma)
+            kl2 = derivateL(t[i] + 0.5*h, t0, L0, sigma)
+
+            kA3 = derivateA(F[i] + 0.5 * h * kF2, Omega[i] + 0.5 * h * kO2)
+            kF3 = derivateF(A[i] + 0.5 * h * kA2, F[i] + 0.5 * h * kF2,
+                            L[i] + 0.5 * h * kl2, Omega[i] + 0.5 * h * kO2)
+            kO3 = derivateOmega(t[i] + 0.5 * h, A[i] + 0.5 * h * kA2,
+                                F[i] + 0.5 * h * kF2, L[i] + 0.5 * h * kl2,
+                                Omega[i] + 0.5 * h * kO2, t0, L0, sigma)
+            kl3 = derivateL(t[i] + 0.5*h, t0, L0, sigma)
+
+            kA4 = derivateA(F[i] + h, Omega[i] + h * kA3)
+            kF4 = derivateF(A[i] + h * kA3, F[i] + h * kF3,
+                            L[i] + h * kl3, Omega[i] + h * kO3)
+            kO4 = derivateOmega(t[i] + h, A[i] + h * kA3,
+                                F[i] + h * kF3, L[i] + h * kl3,
+                                Omega[i] + h * kO3, t0, L0, sigma)
+
+            A[i+1] = A[i] + (kA1 + 2*kA2 + 2*kA3 + kA4) * h/6
+            F[i+1] = F[i] + (kF1 + 2*kF2 + 2*kF3 + kF4) * h/6
+            Omega[i+1] = Omega[i] + (kO1 + 2*kO2 + 2*kO3 + kO4) * h/6
+        
+        
+        return A, F, L, Omega
+
+
+def Bulk(t0, L0, sigma, points_number, step_size):
     A_initial_values = np.arange(3.5, 7, 0.5)
-    F_initial_values = np.arange(0.4, 0.9, 0.1)
+    F_initial_values = np.arange(0.1, 0.9, 0.1)
     Omega_initial_values = np.arange(-0.3, 0.3, 0.1)
     t = np.arange(0, points_number * step_size, step_size)
 
     for a0 in A_initial_values:
         for f0 in F_initial_values:
             for omega0 in Omega_initial_values:
+                try:
+                    A, F, L, Omega = get_results(points_number, step_size, a0, f0, omega0, t, t0, L0, sigma)
 
-                A, F, L, Omega = get_results(points_number, step_size, a0, f0, omega0, t, t0, L0, sigma)
+                    # M = (1-F)*A/2
+                    plt.plot(t, Omega)
+                except FloatingPointError:
+                    print("Number out of range, please try with a smaller"+
+                        "step size and/or different initial conditions")
 
-                # M = (1-F)*A/2
-                plt.plot(t, Omega)
 
             plt.title(r"$\Omega$; A(0)=" + str(np.around(a0, decimals=1)) 
                 + r"; F(0)=" + str(np.around(f0, decimals=1)))    
@@ -52,12 +89,6 @@ def main(a0, f0, omega0, t0, L0, sigma, points_number, step_size):
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-A", "--a0", default=5,
-                    help="Starting value for A(t)", type=positive_float)
-parser.add_argument("-F", "--f0", default=0.5,
-                    help="Starting value for F(t)", type=restricted_F)
-parser.add_argument("-O", "--omega0", default=0,
-                    help="Starting value for Omega(t)", type=restricted_Omega)
 parser.add_argument("-s", "--step_size", default=0.01,
                     help="Step size for calculus", type=positive_float)
 parser.add_argument("-pn", "--points_number", default=10000,
@@ -69,4 +100,4 @@ parser.add_argument("-L", "--L0", default=0.05,
 parser.add_argument("-t", "--t0", default=10,
                     help="Time when gaussian will reach its maximum value", type=float)                                                                            
 args = parser.parse_args()
-main(args.a0, args.f0, args.omega0, args.t0, args.L0, args.sigma, args.points_number, args.step_size)
+Bulk(args.t0, args.L0, args.sigma, args.points_number, args.step_size)

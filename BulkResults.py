3a977cc4bdb429f4676af4e85e209b87b167f40e
FDF94 <fdelfedele@gmail.com>
2019-06-30 18:27:15 -0300

    Now M(0) is always 0 in bulk results

--- BulkResults.py
+++ BulkResults.py
@@ -11,29 +11,28 @@ import argparse
 
 def Bulk(t0, L0, sigma, points_number, step_size):
     A_initial_values = np.arange(3.5, 7, 0.5)
-    F_initial_values = np.arange(0.1, 0.9, 0.1)
     Omega_initial_values = np.arange(-0.4, 0.5, 0.1)
     t = np.arange(0, points_number * step_size, step_size)
 
     for a0 in A_initial_values:
-        for f0 in F_initial_values:
-            for omega0 in Omega_initial_values:
-                try:
-                    A, F, L, Omega = get_results(points_number, step_size, a0, f0, omega0, t, t0, L0, sigma)
+        f0 = 1 - 2/a0
+        for omega0 in Omega_initial_values:
+            try:
+                A, F, L, Omega = get_results(points_number, step_size, a0, f0, omega0, t, t0, L0, sigma)
 
-                    # M = (1-F)*A/2
-                    plt.plot(t, Omega)
-                except FloatingPointError:
-                    print("Number out of range, please try with a smaller"+
-                        "step size and/or different initial conditions")
+                # M = (1-F)*A/2
+                plt.plot(t, Omega)
+            except FloatingPointError:
+                print("Number out of range, please try with a smaller"+
+                    "step size and/or different initial conditions")
 
 
-            plt.title(r"$\Omega$; A(0)=" + str(np.around(a0, decimals=1)) 
-                + r"; F(0)=" + str(np.around(f0, decimals=1)))    
-            plt.savefig("$omega-a0_" + str(np.around(a0, decimals=1)) 
-                + "-f0_" + str(np.around(f0, decimals=1)) + ".png")
-            # plt.show()
-            plt.clf()
+        plt.title(r"$\Omega$; A(0)=" + str(np.around(a0, decimals=1)) 
+            + r"; F(0)=" + str(np.around(f0, decimals=1)))    
+        plt.savefig("$omega-a0_" + str(np.around(a0, decimals=1)) 
+            + "-f0_" + str(np.around(f0, decimals=1)) + ".png")
+        # plt.show()
+        plt.clf()
 
 
 parser = argparse.ArgumentParser()
